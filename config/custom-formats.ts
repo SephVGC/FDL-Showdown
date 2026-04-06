24f28b690a94edb5c60b98765b2fa6b43ed37c23
SephVGC <Sspencer6@outlook.com>
2026-04-06 17:03:39 -0400

    Update FDL Nuzlocke to VGC-style doubles

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -7,8 +7,13 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	},
 	{
 		name: "[Gen 9] FDL Nuzlocke",
-		desc: `All Pok&eacute;mon can use any move or ability, with no learnset restrictions.`,
+		desc: `VGC-style doubles (Lv. 50, bring 4 pick 2). All Pok&eacute;mon can use any move or ability, with no learnset restrictions.`,
 		mod: 'freemoves',
-		ruleset: ['Species Clause', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Endless Battle Clause'],
+		gameType: 'doubles',
+		ruleset: [
+			'Team Preview', 'Species Clause', 'Nickname Clause', 'Item Clause = 1',
+			'Adjust Level Down = 50', 'Picked Team Size = Auto',
+			'HP Percentage Mod', 'Cancel Mod', 'VGC Timer',
+		],
 	},
 ];
